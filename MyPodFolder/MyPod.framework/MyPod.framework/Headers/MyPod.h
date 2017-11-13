8234acefdf1d5f15218e62e7897549454989cbfd
Martin Tyberg <martin@maptext.com>
2017-11-13 15:37:26 -0500

    test

---
+++ MyPodFolder/MyPod.framework/MyPod.framework/Headers/MyPod.h
@@ -0,0 +1,19 @@
+//
+//  MyPod.h
+//  MyPod
+//
+//  Created by Martin Tyberg on 11/7/17.
+//  Copyright © 2017 MapText. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyPod.
+FOUNDATION_EXPORT double MyPodVersionNumber;
+
+//! Project version string for MyPod.
+FOUNDATION_EXPORT const unsigned char MyPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyPod/PublicHeader.h>
+
+
